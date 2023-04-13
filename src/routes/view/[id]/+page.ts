babbe5c30d4b79b16623c7e2515ae645f7638dfd
Rick Henry <rickhenry@rickhenry.dev>
2023-04-12 21:54:41 -0400

    Switch from isr and rust to edge direct download
    switch to downloading the json files from b2 directly in the page load
    function using fetch so it can run fast on the edge and only needs one
    cold start instead of two. removed giant aws-sdk that's not needed.
    dramatically improved load times
    
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- src/routes/view/[id]/+page.ts
+++ src/routes/view/[id]/+page.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit';
 import { NotesData } from '$lib/notesData';
 import type { PageLoad } from './$types';
-import { PUBLIC_ENDPOINT } from '$env/static/public';
+// import { PUBLIC_ENDPOINT } from '$env/static/public';
 
 export const load = (async ({ params, fetch }) => {
   if (!params.id) {
@@ -12,7 +12,11 @@ export const load = (async ({ params, fetch }) => {
   try {
     // This is a hack for development. relative url function calls don't work here
     // in development, but should work in production.
-    const response = await fetch(`${PUBLIC_ENDPOINT}/api/view?id=${params.id}`);
+    const response = await fetch(`https://f000.backblazeb2.com/file/student-lesson-notes/${params.id}.json`, {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
     const data = await response.json();
     if (!('warmups' in data)
       || !('exercises' in data)
@@ -31,10 +35,10 @@ export const load = (async ({ params, fetch }) => {
   }
 }) satisfies PageLoad;
 
-export const config = {
-  isr: {
-    // Keep for a month
-    expiration: 60 * 60 * 24 * 30,
-  },
-  runtime: 'nodejs18.x',
-}
+// export const config = {
+//   isr: {
+//     // Keep for a month
+//     expiration: 60 * 60 * 24 * 30,
+//   },
+//   runtime: 'nodejs18.x',
+// }
